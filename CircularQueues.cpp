60859ffab8aa21b2c22879457ca59ed509c875f0
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-31 14:56:30 +0700

    mengubah posisi REAR yang berada di posisi akhir array kembali ke awal array

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -28,3 +28,13 @@ public:
             FRONT = 0;
             REAR = 0;
         }
+        else{
+            //jika rearberada di posisi terakhir kembali ke awall array
+            if(REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
