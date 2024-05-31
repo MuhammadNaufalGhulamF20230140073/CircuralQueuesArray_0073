9acf518c5f4dbe2c770a487a89f2b1b7e58db38a
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-31 15:05:58 +0700

    membuat element yang di hapus di akhir array kembali ke awall array

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -46,3 +46,17 @@ public:
         }
         cout << "\nThe element deleted from the queue is : " << queue_array[FRONT] << "\n";
 
+        //cek  jika antrian hanya memiliki satu element
+        if (FRONT == REAR){
+            FRONT = -1;
+            REAR = -1;
+        }
+        else {
+            //jika element yg di hapus berada di array tekhir kembali ke awal array
+            if(FRONT == max - 1)
+                FRONT = 0;
+            else    
+                FRONT = FRONT + 1;
+        }
+    }
+
