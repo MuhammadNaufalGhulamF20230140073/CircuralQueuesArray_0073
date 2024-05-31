d6dfa52b8e2f79956bbebc1d5c65eadb63a44286
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-31 14:48:20 +0700

    jika penuh mencetak "queue overflow"

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -19,3 +19,7 @@ public:
 
         //cek antrian penuh
         if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR + 1)){
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
